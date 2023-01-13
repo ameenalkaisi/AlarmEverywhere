a61e5471d12bd580efbb4b841145cd9a75eef780
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-01-13 16:40:53 -0500

    added alarms and linked users to alarms in the backend and in the database

--- back-end/server.go
+++ back-end/server.go
@@ -27,6 +27,20 @@ type User struct {
 	VerificationCode string
 }
 
+type Alarm struct {
+	ID uint `gorm:"primarykey"`
+
+	Date       time.Time `gorm:"not null"`
+	Recurrence string    `gorm:"type:ENUM('HOURLY','DAILY','WEEKLY','BIWEEKLY','MONTHLY','YEARLY','NONE');not null;default:'NONE'"`
+}
+
+type UserAlarm struct {
+	UserId  uint
+	AlarmId uint
+	User    User  `gorm:"foreignKey:UserId"`
+	Alarm   Alarm `gorm:"foreignKey:AlarmId"`
+}
+
 type UserSignupRequest struct {
 	Name  string `json:"name" validate:"required"`
 	Email string `json:"email" validate:"required,email"`
@@ -44,6 +58,15 @@ type UserMeResponse struct {
 	Email    string
 	Role     string
 	Verified bool
+}
+
+type UserAlarmRequest struct {
+	Alarms []AlarmType
+}
+
+type AlarmType struct {
+	DateString string
+	Recurrence string
 }
 
 // seems like we can use this validator for typed struct we make
@@ -89,7 +112,7 @@ func GetUser(next echo.HandlerFunc) echo.HandlerFunc {
 
 		var user User
 
-		result := db.First(&user, "id = ?", fmt.Sprint(sub))
+		result := db.Table("users").First(&user, "id = ?", fmt.Sprint(sub))
 		if result.Error != nil {
 			return c.JSON(http.StatusForbidden, "user no longer exists")
 		}
@@ -111,7 +134,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	err = db.AutoMigrate(&User{})
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(&Alarm{})
+	if err != nil {
+		panic(err)
+	}
+	err = db.AutoMigrate(&UserAlarm{})
 	if err != nil {
 		panic(err)
 	}
@@ -153,7 +185,7 @@ func main() {
 			Verified:         false,
 			VerificationCode: encodedRandomCode,
 		}
-		result := db.Create(&newUser)
+		result := db.Table("users").Create(&newUser)
 		if result.Error != nil {
 			return c.JSON(http.StatusBadRequest, result.Error.Error())
 		}
@@ -165,11 +197,76 @@ func main() {
 		return c.JSON(http.StatusOK, "user created")
 	})
 
+	e.GET("/alarms", func(c echo.Context) (err error) {
+		var alarms []Alarm
+
+		curUser := c.Get("currentUser").(User)
+		if result := db.Table("alarms").Select("alarms.date, alarms.recurrence").Joins("JOIN user_alarms on user_alarms.alarm_id = alarms.id").Where("user_id = ?", curUser.ID).Find(&alarms); result.Error != nil {
+			fmt.Println(result.Error.Error())
+
+			return c.JSON(http.StatusInternalServerError, "could not find user's alarms")
+		}
+
+		return c.JSON(http.StatusOK, alarms)
+	}, GetUser)
+
+	// this always sets them all at once
+	e.POST("/alarms/create", func(c echo.Context) (err error) {
+		user := c.Get("currentUser").(User)
+		// get all the values first
+
+		alarmRequest := new(UserAlarmRequest)
+
+		if err := c.Bind(alarmRequest); err != nil {
+			fmt.Println(err.Error())
+			return c.JSON(http.StatusBadRequest, "format of request is incorrect")
+		}
+
+		if result := db.Table("user_alarms").Delete(&Alarm{}, "user_id = ?", user.ID); result.Error != nil {
+			fmt.Println(result.Error.Error())
+
+			return c.JSON(http.StatusInternalServerError, "Could not delete already set alarms")
+		}
+
+		userAlarms := make([]UserAlarm, 0, len(alarmRequest.Alarms))
+		for _, alarm := range alarmRequest.Alarms {
+			date, err := time.Parse("2006-01-02T15:04:05-0700", alarm.DateString)
+			if err != nil {
+				fmt.Println(err.Error())
+				return c.JSON(http.StatusBadRequest, "format of date is incorrect")
+			}
+
+			newAlarm := Alarm{
+				Date:       date,
+				Recurrence: alarm.Recurrence,
+			}
+
+			if result := db.Table("alarms").FirstOrCreate(&newAlarm,
+				"date = @date AND recurrence = @recur",
+				map[string]interface{}{"date": newAlarm.Date, "recur": newAlarm.Recurrence}); result.Error != nil {
+				fmt.Println(result.Error.Error())
+
+				return c.JSON(http.StatusInternalServerError, "could not create alarm")
+			}
+
+			userAlarms = append(userAlarms, UserAlarm{UserId: user.ID, User: user, AlarmId: newAlarm.ID, Alarm: newAlarm})
+		}
+
+		if result := db.Table("user_alarms").Create(userAlarms); result.Error != nil {
+			return c.JSON(http.StatusInternalServerError, "could not add the new alarms in")
+		}
+
+		// if things fail after this, the user will lose all their alarms
+		// todo, figure out how to deal with this
+
+		return c.JSON(http.StatusOK, "alarms successfully synced")
+	}, GetUser)
+
 	e.GET("/verify-email/:verificationCode", func(c echo.Context) error {
 		code := utils.Encode(c.Param("verificationCode"))
 
 		var updatedUser User
-		result := db.First(&updatedUser, "verification_code = ?", code)
+		result := db.Table("users").First(&updatedUser, "verification_code = ?", code)
 
 		if result.Error != nil {
 			return c.JSON(http.StatusBadRequest, result.Error.Error())
@@ -177,7 +274,7 @@ func main() {
 
 		updatedUser.VerificationCode = ""
 		updatedUser.Verified = true
-		if result := db.Save(updatedUser); result.Error != nil {
+		if result := db.Table("users").Save(updatedUser); result.Error != nil {
 			return c.JSON(http.StatusInternalServerError, "could not update to a verified account")
 		}
 
@@ -188,7 +285,7 @@ func main() {
 		email := c.Param("email")
 
 		var userToUpdate User
-		result := db.First(&userToUpdate, "email = ?", email)
+		result := db.Table("users").First(&userToUpdate, "email = ?", email)
 		if result.Error != nil {
 			return c.JSON(http.StatusBadRequest, "email never registered under an account")
 		}
@@ -201,7 +298,7 @@ func main() {
 		encodedRandomCode := utils.Encode(randomCode)
 
 		userToUpdate.VerificationCode = encodedRandomCode
-		result = db.Save(userToUpdate)
+		result = db.Table("users").Save(userToUpdate)
 		if result.Error != nil {
 			return c.JSON(http.StatusInternalServerError, "could not update the verification code, please try again later")
 		}
@@ -222,7 +319,7 @@ func main() {
 
 		// find user and password from the database
 		var foundUser User
-		if result := db.First(&foundUser, "email = ?", strings.ToLower(userLogin.Email)); result.Error != nil {
+		if result := db.Table("users").First(&foundUser, "email = ?", strings.ToLower(userLogin.Email)); result.Error != nil {
 			fmt.Println(result.Error.Error())
 			return c.JSON(http.StatusBadRequest, result.Error.Error())
 		}
@@ -253,7 +350,7 @@ func main() {
 		cookie := http.Cookie{
 			Name:     "token",
 			Value:    token,
-			MaxAge:   maxAge,
+			MaxAge:   maxAge * 60,
 			Secure:   false,
 			HttpOnly: true,
 		}
